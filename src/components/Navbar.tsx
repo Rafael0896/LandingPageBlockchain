1a5d6f686c46696d04dbf85fe5322064f2c3e09c
Rafael0896 <realvarez54@misena.edu.co>
2025-08-09 16:58:07 -0500

    Utdate

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,11 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="nav-logo">
-                <a href="/">Blockchain Explained</a>
+                <a href="/">Blockchain</a>
             </div>
             <ul className="nav-links">
-                <li><a href="#hero" className="nav-item">Inicio</a></li>
+                <li><a href="/" className="nav-item">Inicio</a></li>
                 <li><a href="#contact" className="nav-item">Contacto</a></li>
-                <li><a href="https://es.react.dev/" target="_blank" rel="noopener noreferrer" className="nav-item">Blog</a></li>
             </ul>
         </nav>
     );
